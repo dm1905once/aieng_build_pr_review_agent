2c26bfd67c5291329e16eb46a8498a051c062f16
danielm <dm1905once@gmail.com>
2026-05-17 20:22:19 -0700

    Phase 2 - Created first function

--- Building a PR Review Agent/task/main.py
+++ Building a PR Review Agent/task/main.py
@@ -1,6 +1,7 @@
 import dotenv
 import os
 from github import Github
+from github import GithubException
 from github import Auth
 
 # Initialize
@@ -13,15 +14,25 @@ username = repo_url.split('/')[-2]
 full_repo_name = f"{username}/{repo_name}"
 
 # Functions
-def get_pr_details(id:int):
-    if git is not None:
+def get_pr_details(pull_id:int) -> str:
+    pr_details = {}
+    try:
         repo = git.get_repo(full_repo_name)
-        pull = repo.get_pull(id)
-        print(pull.title)
-    else:
-        return None
-    git.close()
+        pull = repo.get_pull(pull_id)
+        pr_details['id'] = pull.id
+        pr_details["repo_name"] = full_repo_name
+        pr_details["author"] = pull.user.login
+        pr_details["title"] = pull.title
+        pr_details["body"] = pull.body
+        pr_details["diff_url"] = pull.diff_url
+        commit_SHAs = []
+        commits = pull.get_commits()
+        for c in commits:
+            commit_SHAs.append(c.sha)
+        pr_details["commit_shas"] = commit_SHAs
+        git.close()
+    except GithubException as e:
+        pr_details['error'] = "Unable to retrieve PR details"
+    return str(pr_details)
 
-get_pr_details(3)
-#file_content = repo.get_contents("main.py").decoded_content.decode('utf-8')
-#print(file_content)
+print(get_pr_details(3))
